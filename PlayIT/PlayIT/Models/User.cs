6d349403542c1efa7ec4b110545a24d8f40ee048
Anbumani Thamilvanan <s.abdullah299@gmail.com>
2017-09-21 00:47:52 +0530

    Login

---
+++ PlayIT/PlayIT/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayIT.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User() { }
+        public User(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+        }
+
+        public bool CheckInformation()
+        {
+            if (!(string.IsNullOrEmpty(this.Username) || this.Username.Equals("")) &&
+                !(string.IsNullOrEmpty(this.Password) || this.Password.Equals("")))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ PlayIT/PlayIT/Views/SigninPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using PlayIT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PlayIT
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SigninPage : ContentPage
+	{
+		public SigninPage ()
+		{
+			InitializeComponent ();
+            Init();
+		}
+
+        void Init()
+        {
+            ActivitySpinner.IsVisible = false;
+            Entry_Username.Completed += (s, e) => Entry_Password.Focus();
+            Entry_Password.Completed += (s, e) => Login(s, e);
+        }
+        
+        void Login(object sender, EventArgs e)
+        {
+            User user = new User(Entry_Username.Text, Entry_Password.Text);
+
+            try
+            {
+                if (user.CheckInformation())
+                {
+                    DisplayAlert("Login", "Login Success", "OK");
+                }
+                else
+                {
+                    DisplayAlert("Login", "Login failed. Invalid Username or Password", "Try Again");
+                }
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Warning", "Username or Password cannot be empty", "Try Again");
+            }
+            
+        }
+	}
+}
